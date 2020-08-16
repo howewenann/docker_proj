7e7b83facd62cce0451da69d4f75d3c05bd256c0
Nicholas <wenann.howe@gmail.com>
2020-08-16 17:00:28 +0800

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A Repo to test deploying a model on docker',
+    author='Nicholas',
+    license='',
+)
